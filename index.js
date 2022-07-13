3a78edd20e8df6c3f6d018faa6bb90cc7dfcdc0c
rochafialvin <rochafi.dev@gmail.com>
2022-07-13 10:51:59 +0700

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const port = 2105;
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+// calling home
+app.get("/", (req, res) => {
+  res.send("API JALAN MZ 🚀");
+});
+
+// error handler
+app.use((error, req, res, next) => {
+  console.log({ error });
+
+  const errorObj = {
+    status: "Error",
+    message: error.message,
+    detail: error,
+  };
+
+  const httpCode = typeof error.code == "number" ? error.code : 500;
+  res.status(httpCode).send(errorObj);
+});
+
+//  running api
+app.listen(port, (error) => {
+  if (error) return console.log({ err: error.message });
+  console.log(`API berhasil running di port ${port}`);
+});
